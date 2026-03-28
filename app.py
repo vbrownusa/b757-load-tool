4e267166b85a143bbad164974cc53921a5bbaea5
vbrownusa <vbrownusa@gmail.com>
2026-03-27 22:11:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -191,23 +191,25 @@ with cols[3]:
 
 st.subheader("Cargo")
 
+cols = st.columns(4)
+
 with cols[0]:
-    c1 = st.number_input("Bin 1", 0)
+    c1 = st.number_input("Bin 1", min_value=0, value=0)
     c1_awu = cargo_awu_by_rule(CARGO_BIN1, c1)
     st.caption(f"AWU: {c1_awu:.1f}")
 
 with cols[1]:
-    c2 = st.number_input("Bin 2", 0)
+    c2 = st.number_input("Bin 2", min_value=0, value=0)
     c2_awu = cargo_awu_by_rule(CARGO_BIN2, c2)
     st.caption(f"AWU: {c2_awu:.1f}")
 
 with cols[2]:
-    c3 = st.number_input("Bin 3", 0)
+    c3 = st.number_input("Bin 3", min_value=0, value=0)
     c3_awu = cargo_awu_by_rule(CARGO_BIN3, c3)
     st.caption(f"AWU: {c3_awu:.1f}")
 
 with cols[3]:
-    c4 = st.number_input("Bin 4", 0)
+    c4 = st.number_input("Bin 4", min_value=0, value=0)
     c4_awu = cargo_awu_by_rule(CARGO_BIN4, c4)
     st.caption(f"AWU: {c4_awu:.1f}")
 

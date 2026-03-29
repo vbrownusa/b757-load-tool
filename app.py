35e1198c427a64400945110da75d9b171f204bfd
vbrownusa <vbrownusa@gmail.com>
2026-03-28 20:29:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -256,4 +256,17 @@ st.write(f"Passengers AWU: {za + zb + zc:.1f}")
 st.write(f"Baggage AWU: {b1_awu + b2_awu + b3_awu + b4_awu:.1f}")
 st.write(f"Cargo AWU: {c1_awu + c2_awu + c3_awu + c4_awu:.1f}")
 
+st.subheader("ZFW")
+
+zfw = (
+    BOW
+    + za + zb + zc
+    + b1_awu + b2_awu + b3_awu + b4_awu
+    + c1_awu + c2_awu + c3_awu + c4_awu
+)
+
+st.write(f"{zfw:.1f}")
+
+
+
 st.divider()
